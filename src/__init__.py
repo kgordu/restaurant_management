f47c8b40108d6b265257a4cd69ad1a16d6ac6138
Kivanc Gordu <kivancgordu@hotmail.com>
2024-07-22 13:00:30 +0000

    Add log and metadata info to __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,21 @@
 from .constants import MENU_ITEMS, TABLES, CUSTOMER_ORDERS, CUSTOMER_BUDGET
 from .restaurant import Restaurant
+import logging
 
-PACKAGE_VERSION = '1.0.0'
+logging.basicConfig(
+  level=logging.INFO,
+  filename='src.log',
+  filemode='a',
+  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+logger.info("Package is initialized")
+
+
+__version__ = "1.0.0"
+__date__ = "22-06-2024"
+__email__ = "kivancgordu@hotmail.com"
+__status__ = "production"
 
 __all__ = ['Restaurant', 'MENU_ITEMS', 'TABLES', 'CUSTOMER_ORDERS', 'CUSTOMER_BUDGET']
